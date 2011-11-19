02644552dfc0b2b3efed16ca0ce32f3cb35ab92b
RobinWu <robin.wu.cn@gmail.com>
2011-11-19 16:05:30 +0800

    add comment

--- test_baidu.rb
+++ test_baidu.rb
@@ -1,4 +1,12 @@
+# Docs:
 # http://code.google.com/p/selenium/wiki/RubyBindings
+# http://code.google.com/p/chromium/downloads/list
+#
+# Support chrome:
+# 1. download laster chrome
+# 2. wget http://chromium.googlecode.com/files/chromedriver_linux32_16.0.902.0.zip
+# 3. unzip chromedriver_linux32_16.0.902.0.zip
+# 4. cp chromedriver /usr/local/bin
 
 require 'rubygems'
 require 'selenium-webdriver'
