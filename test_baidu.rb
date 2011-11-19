bece89204f576306cd2e8a41fc4865d93bba5170
RobinWu <robin.wu.cn@gmail.com>
2011-11-19 13:22:10 +0800

    test_baidu.rb

---
+++ test_baidu.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :firefox
+driver.get "http://www.baidu.com"
+
+element = driver.find_element :id => "kw"
+element.send_keys "selenium"
+element.submit
+
+puts "Page title is #{driver.title}"
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "cheese!" }
+
+puts "Page title is #{driver.title}"
+driver.quit
